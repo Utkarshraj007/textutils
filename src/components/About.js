3976e56a8f209bbdb82bcfad0bd1560b915afd26
Utkarshraj007 <59512749+Utkarshraj007@users.noreply.github.com>
2022-06-01 09:46:39 +0530

    my first commit

---
+++ src/components/About.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+export default function About(){
+    const[myStyle,setMyStyle]=useState({
+         color:"black",
+        backgroundColor:'white'
+    })
+    const [btnText, setbtnText] = useState("Enable Dark Mode")
+    const toggleStyle=()=>{
+        if(myStyle.color === 'black')
+        {
+            setMyStyle({
+                 color:"white",
+                backgroundColor:'black',
+                border:'1px solid white'
+            })
+            setbtnText("Enable Light Mode");
+        }
+        else{
+            {
+                setMyStyle({
+                     color:"black",
+                    backgroundColor:'white'
+                })
+                setbtnText("Enable Dark Mode");
+            }
+        }
+         
+    }
+  return (
+    <div className='container' style={myStyle}>
+    <h1 className='my-3'>About Us</h1>
+        <div className="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingOne">
+      <button className="accordion-button" type="button"  data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <strong>Analyze the text</strong>
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div className="accordion-body" style={myStyle}>
+        TextUtls gives you wat to analze your text quickly and efficiently.Be it changing the text to uppercase or lowercase 
+        or copying the text to clipboard,clearing the text area and so on.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingTwo">
+      <button className="accordion-button collapsed" type="button"  data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <strong>Easy to Use</strong>
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div className="accordion-body" style={myStyle}>
+        It is a free character counter tool.We can count the no of characters, word and sentences.So it becomes very convenient to use it in our day to day life.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="headingThree">
+      <button className="accordion-button collapsed" type="button"  data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <strong>Browser Compatibility</strong>
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div className="accordion-body" style={myStyle}>
+        This web tool can work on any browser.It is compatible on any browser like chrome,firefox,safari and so on.
+      </div>
+    </div>
+  </div>
+</div>
+<div className="container my-2">
+<button onClick={toggleStyle} type="button" className="btn btn-primary my-3">{btnText}</button>
+</div>
+
+    </div>
+  )
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+export default function (props) {
+  
+  const[text,setText]=useState('')
+  const handleOnChange=(event)=>{
+      setText(event.target.value);
+  }
+  const handleUpClick=()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showalert("Converted to UpperCase","primary")
+  }
+  const handleLoClick=()=>{ 
+    setText(text.toLowerCase());
+    props.showalert("Converted to LowerCase","primary")
+  }
+const handleClearClick=()=>{ 
+  setText('');
+  props.showalert("TextArea Cleared","danger")
+}
+  const handleCopy = ()=>{
+    var text=document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showalert("Copied to clipboard","warning")
+  }
+  const handleExtraSpaces = ()=>{
+    let newText=text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showalert("Extra spaces removed","success")
+  }
+  function firstCap(){
+    let newText = text.toLowerCase() 
+    let newText2 = newText.charAt(0).toUpperCase() + newText.slice(1);
+    setText(newText2)
+    props.showalert("First letter capital","success")
+}
+const vowelCheck = ()=>{
+  const count = text.match(/[aeiou]/gi).length;
+  alert(`You have ${count?count:0} no of vowels`)
+}
+
+  return (
+    <div className="container" style={{color:props.mode == 'dark'?'white':'#042743'}}>
+    <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control" value={text} onChange={handleOnChange}
+          style={{backgroundColor:props.mode == 'dark'?'#13466e':'white',color:props.mode == 'dark'?'white':'black'}}
+          id="myBox"
+          rows="8"
+        ></textarea>
+      </div>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleClearClick}>Clear the text area</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy the text area</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove extra Spaces</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={firstCap}>First letter Uppercase</button>
+      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={vowelCheck}>Vowel Check</button>
+      <div className="container my-3" style={{color:props.mode == 'dark'?'white':'black'}}>
+        <h2>Your text summary</h2>
+        <p>{text.split(/\s+/).filter(element=> element.length!=0).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(' ').filter(element=>element.length!=0).length} minutes read</p>
+        <p>{text.split(/[".!?"]/).length-1} :No of Sentences</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text:"Enter some text to preview"}</p>
+      </div>
+    </div>
+  );
+}
